b62e80643b86496912046f339ab52858571da639
kuripasanda <ytkuripachanda@gmail.com>
2023-03-05 03:16:49 +0900

    init commit

---
+++ src/main/java/com/tatamiserver/economyutilsapi/EconomyUtilsAPI.java
@@ -0,0 +1,17 @@
+package com.tatamiserver.economyutilsapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EconomyUtilsAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/tatamiserver/economyutilsapi/api/EconomyUtilsAPI.java
@@ -0,0 +1,9 @@
+package com.tatamiserver.economyutilsapi.api;
+
+public class EconomyUtilsAPI {
+
+    public static String test() {
+        return "test";
+    }
+
+}
